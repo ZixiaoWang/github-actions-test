7ab2f4cbd623ef0b8fe0163aab34b92e2e61aba3
ZixiaoWang <www.wangzixiao@gmail.com>
2020-02-12 17:05:54 +0800

    Switched to hash router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import React from 'react';
 import { render } from 'react-dom';
-import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Redirect, BrowserRouter, HashRouter } from 'react-router-dom';
 import { PostList } from './pages/post-list.page';
 import { PostContent } from './pages/post-content.page';
 import { UserDetail } from './pages/user-detail.page';
@@ -10,14 +10,14 @@ import { PostService, UserService, CommentService } from './services';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Switch>
                 <Route path="/posts" exact component={ PostList } />
                 <Route path="/posts/:postId" exact component={ PostContent } />
                 <Route path="/user/:userId" exact component={ UserDetail } />
                 <Redirect to="/posts" />
             </Switch>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
