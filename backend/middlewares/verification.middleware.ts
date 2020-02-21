577a5159df045dfdf2cb61747f6657dfdf7ec154
ZixiaoWang <www.wangzixiao@gmail.com>
2020-02-21 17:16:58 +0800

    enabled client auth

--- backend/middlewares/verification.middleware.ts
+++ backend/middlewares/verification.middleware.ts
@@ -8,18 +8,18 @@ const whitelist: string[] = [
 export const verificationMiddleware = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const request: any = req;
 
-    // if (request.method.toLowerCase() === 'post' && request.path === '/login') {
-    //     res.cookie('USER', 'user', { signed: true });
-    //     res.status(200);
-    //     res.send(true);
-    // } else {
-    //     if (request.signedCookies['USER'] === 'user') {
-    //         next();
-    //     } else {
-    //         res.status(200);
-    //         res.sendFile(path.resolve(__dirname, '../../../statics/login.html'));
-    //     }
-    // }
+    if (request.method.toLowerCase() === 'post' && request.path === '/login') {
+        res.cookie('USER', 'user', { signed: true });
+        res.status(200);
+        res.send(true);
+    } else {
+        if (request.signedCookies['USER'] === 'user') {
+            next();
+        } else {
+            res.status(200);
+            res.sendFile(path.resolve(__dirname, '../../../statics/login.html'));
+        }
+    }
 
-    next();
+    // next();
 }
